29c367ff6da6460c01b193ce62ae9bb33087ed69
Pierpaolo Calanna <p.calanna@gmail.com>
2025-10-21 10:10:31 +0200

    Add saving of Monte Carlo samples to data_writer
    The save_analysis_results function now accepts simulation_samples and writes them to a separate JSON file alongside bootstrap samples. This supports storing Monte Carlo results for further analysis.

--- lib/data_writer.py
+++ lib/data_writer.py
@@ -8,7 +8,9 @@ data_out = Path("./data_out")
 
 def save_analysis_results(
     data_dict: dict[str, Any],
-    bootstrap_samples: list[Any]) -> None:
+    bootstrap_samples: list[Any],
+    simulation_samples: list[Any],
+    ) -> None:
     """
     Save analysis results and bootstrap estimates to JSON files.
 
@@ -17,15 +19,18 @@ def save_analysis_results(
     data_dict : dict
         Dictionary containing data
 
-    bootstrap_samples : dict
-        Dictionary containing bootstrap estimates
+    bootstrap_samples : list
+        list containing bootstrap samples
+
+    simulation_samples: list
+        list containing montecarlo samples
 
     Returns:
     --------
     None
     """
 
-    # Extract metric name from data dictionary
+    # Extract from data dictionary
     metric_config: dict[str, Any] = data_dict.get("metric_config", {})
     metric_name = metric_config.get("name", "unknown_metric")
 
@@ -46,8 +51,14 @@ def save_analysis_results(
         orjson_options = orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_INDENT_2
         f.write(orjson.dumps(data_dict, option=orjson_options).decode("utf-8"))
 
-    # Write bootstrap estimates to separate JSON file
+    # Write bootstrap samples to JSON file
     bootstrap_output_path = output_path / f"{metric_name}_bootstrap_samples.json"
     with bootstrap_output_path.open("w") as f:
         orjson_options = orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_INDENT_2
         f.write(orjson.dumps(bootstrap_samples, option=orjson_options).decode("utf-8"))
+
+    # Write montecarlo samples to JSON file
+    motnecarlo_output_path = output_path / f"{metric_name}_montecarlo_samples.json"
+    with motnecarlo_output_path.open("w") as f:
+        orjson_options = orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_INDENT_2
+        f.write(orjson.dumps(simulation_samples, option=orjson_options).decode("utf-8"))
