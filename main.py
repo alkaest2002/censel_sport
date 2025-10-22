256d3af2b1a9468456afa347c132f82000dc1683
Pierpaolo Calanna <p.calanna@gmail.com>
2025-10-22 19:44:47 +0200

    Wrap main analysis loop in try-except block
    Added a try-except block around the main analysis loop to catch and print exceptions, improving error handling and preventing the script from crashing silently during processing.

--- main.py
+++ main.py
@@ -13,68 +13,72 @@ from lib.data_plot import create_plots
 from lib.data_save import save_analysis_results
 from lib.data_standardize import compute_standard_scores
 
-# Iterate over all metric configuration files in data_in folder
-for metric_config_path in Path("./data_in").glob("*.json"):
+try:
+        
+    # Iterate over all metric configuration files in data_in folder
+    for metric_config_path in Path("./data_in").glob("*.json"):
 
-    # Open metric configuration file
-    with metric_config_path.open("r") as f:
+        # Open metric configuration file
+        with metric_config_path.open("r") as f:
 
-        # Parse metric configuration
-        metric_config: dict[str, Any] = orjson.loads(f.read())
+            # Parse metric configuration
+            metric_config: dict[str, Any] = orjson.loads(f.read())
 
-        # Skip if included in analysis is set to False
-        if metric_config.get("include_in_analysis", False) is False:
-            continue
+            # Skip if included in analysis is set to False
+            if metric_config.get("include_in_analysis", False) is False:
+                continue
 
-    #################################################################################
-    # Load data
-    ################################################################################
-    print("1. Loading data...")
-    data_dict: dict[str, Any] = load_data(metric_config=metric_config)
+        #################################################################################
+        # Load data
+        ################################################################################
+        print("1. Loading data...")
+        data_dict: dict[str, Any] = load_data(metric_config=metric_config)
 
-    ############################################################################################
-    # Clean data
-    ############################################################################################
-    print("2. Cleaning data...")
-    data_dict = clean_data(data_dict)
+        ############################################################################################
+        # Clean data
+        ############################################################################################
+        print("2. Cleaning data...")
+        data_dict = clean_data(data_dict)
 
-    #############################################################################################
-    # Compute bootstrap percentiles
-    #############################################################################################
-    print("3. Computing bootstrap percentiles...")
-    data_dict, bootstrap_samples = compute_bootstrap_percentiles(data_dict=data_dict)
+        #############################################################################################
+        # Compute bootstrap percentiles
+        #############################################################################################
+        print("3. Computing bootstrap percentiles...")
+        data_dict, bootstrap_samples = compute_bootstrap_percentiles(data_dict=data_dict)
 
-    ##############################################################################################
-    # Apply standardization to data
-    ##############################################################################################
-    print("4. Applying standardization...")
-    data_dict = compute_standard_scores(data_dict)
+        ##############################################################################################
+        # Apply percentile-based standardization to data
+        ##############################################################################################
+        print("4. Applying standardization...")
+        data_dict = compute_standard_scores(data_dict)
 
-    ###############################################################################################
-    # Fit theoretical distributions to data
-    ###############################################################################################
-    print("5. Fitting theoretical distributions...")
-    fitter = DistributionFitter(data_dict)
-    data_dict = fitter.fit_distributions()
+        ###############################################################################################
+        # Fit theoretical distributions to data
+        ###############################################################################################
+        print("5. Fitting theoretical distributions...")
+        fitter = DistributionFitter(data_dict)
+        data_dict = fitter.fit_distributions()
 
-    ###############################################################################################
-    # Montecarlo simulation
-    ###############################################################################################
-    print("6. Performing Montecarlo simulation...")
-    data_dict, simulation_samples = monte_carlo_validation(data_dict)
+        ###############################################################################################
+        # Perform Montecarlo simulation
+        ###############################################################################################
+        print("6. Performing Montecarlo simulation...")
+        data_dict, simulation_samples = monte_carlo_validation(data_dict)
 
-    ###############################################################################################
-    # Save plots
-    ###############################################################################################
-    print("7. Saving plots...")
-    data_dict = create_plots(data_dict)
+        ###############################################################################################
+        # Create plots
+        ###############################################################################################
+        print("7. Saving plots...")
+        data_dict = create_plots(data_dict)
 
-    ##############################################################################################
-    # Save results
-    ##############################################################################################
-    print("8. Saving results...")
-    save_analysis_results(
-        data_dict=data_dict,
-        bootstrap_samples=bootstrap_samples,
-        simulation_samples=simulation_samples,
-    )
+        ##############################################################################################
+        # Save results
+        ##############################################################################################
+        print("8. Saving results...")
+        save_analysis_results(
+            data_dict=data_dict,
+            bootstrap_samples=bootstrap_samples,
+            simulation_samples=simulation_samples,
+        )
+except Exception as e:  # noqa: BLE001
+    print(e)
